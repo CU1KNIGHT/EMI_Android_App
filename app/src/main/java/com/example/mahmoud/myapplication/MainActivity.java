0e4d9614f51e960430331c96b65e6a480d1c286e
CU1KNIGHT <mah2009moud@hotmail.com>
2018-12-06 23:15:07 +0100

    i have changed the code and rest set the methods
    Signed-off-by: CU1KNIGHT <mah2009moud@hotmail.com>

--- app/src/main/java/com/example/mahmoud/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mahmoud/myapplication/MainActivity.java
@@ -12,131 +12,101 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    //e ist die Eingabe vom User
+    //Text ist die Ausgabe
+
+
 EditText e   ;
+    TextView Quersumme;
 TextView Text;
 Button b;
 Button Berchne;
 TextView fabView;
-    float summe;
-    ArrayList<String> listNumbers= new ArrayList<>();
+    int summe;
+    ArrayList<String> listNumbers = new ArrayList<>();
 
-   /* System.out.print( + summe + " summe: ");
-    int n = summe, t1 = 0, t2 = 1;
-        System.out.print("First " + n + " terms: ");
-
-        for (int i = 1; i <= n; ++i)
-    {
-        System.out.print(t1 + " + ");
-
-        int sum = t1 + t2;
-        t1 = t2;
-        t2 = sum;
-    }*/
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        e   = (EditText) findViewById(R.id.editText);
-        Text  = (TextView ) findViewById( R.id.textView);
-        b= (Button) findViewById(R.id.button);
-        fabView   = (TextView ) findViewById( R.id.fabView);
-fabView.setMovementMethod(new ScrollingMovementMethod());
-
-//+++++++++++++++++++++++++++++++++++++++++++++[   =   ]+++++++++++++++++++
+    void calculateDigitSum() {//+++++++++++++++++++++++++++++++++++++++++++++[   =   ]+++++++++++++++++++
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String num= e.getText().toString();
-                Float f= Float.parseFloat(num);
-              //  float f= Math.round(c);
-               // Integer f= Integer.parseInt(num);
-                 summe = 0;
-          while (0 != f) {
-            // addiere die letzte ziffer der uebergebenen zahl zur summe
-             summe = summe + (f % 10);
-            // entferne die letzte ziffer der uebergebenen zahl
-            f = f / 10;
+                String num = e.getText().toString();
+             
+                Integer f = Integer.parseInt(num);
+                summe = 0;
+                while (0 != f) {
+                    // addiere die letzte ziffer der uebergebenen zahl zur summe
+                    summe = summe + (f % 10);
+                    // entferne die letzte ziffer der uebergebenen zahl
+                    f = f / 10;
 
-        }
+                }
 
-                //int s=Integer.parseInt(num);
-               Text.setText(String.valueOf(/*Math.round(*/summe)/*)*/);
+                
+                Text.setText(String.valueOf(/*Math.round(*/summe)/*)*/);
 
             }
         });
-//+++++++++++++++++++++++++++++++++++++++++++Brechne
-        Berchne= (Button) findViewById(R.id.Berchne);
+    }
+
+    void calculateFibonacciRow() {//+++++++++++++++++++++++++++++++++++++++++++Brechne
+        Berchne = findViewById(R.id.Berchne);
         Berchne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 listNumbers.clear();
                 fabView.setText("");
-                int s= (int) summe;
+                int s = summe;
 
 
                 int n = s, t1 = 0, t2 = 1;
 
 
-                for (int i = 1; i <= s; ++i)
-                {
-                  //  fabView.setText(t1 +"\n");
-
-
-
-                   /* System.getProperty("line.separator");
-                    for( i=0 ; i < listNumbers.size();i++){
-                        fabView.setText(listNumbers.get(i));
-
-                        Log.d( " value",listNumbers.get(i));
-                    }
-*/
+                for (int i = 1; i <= s; ++i) {
 
 
                     listNumbers.add(String.valueOf(t1));
-                    int sum = t1 + t2 ;
-                    String sumS= sum+"\n";
+                    int sum = t1 + t2;
+                    String sumS = sum + "\n";
                     t1 = t2;
                     t2 = sum;
-                  //  fabView.setText(sumS);
-                  //  fabView.setText("sum\r\nNext line");
 
 
+                }
+                if (listNumbers.size() > 30) {
 
-
-            }
-                if (listNumbers.size()> 30) {
-
-                    for( int x=0 ; x <= 30;++x){
-                        // fabView.setText(listNumbers.get(f)+" \n");
-                        String s4= listNumbers.get(x) + "\r\n";
+                    for (int x = 0; x <= 30; ++x) {
+                        String s4 = listNumbers.get(x) + "\r\n";
                         fabView.append(s4);
                         Log.d("Fab n", s4);
-                    }}else{
-                for( int f=0 ; f < listNumbers.size();f++){
-               // fabView.setText(listNumbers.get(f)+" \n");
-                    String s3= listNumbers.get(f) + "\r\n";
-                    fabView.append(s3);}}
-
-
-
+                    }
+                } else {
+                    for (int f = 0; f < listNumbers.size(); f++) {
+                        String s3 = listNumbers.get(f) + "\r\n";
+                        fabView.append(s3);
+                    }
+                }
 
 
             }
         });
+    }
 
-//String num= e.getText().toString();
-     //   Float f= Float.parseFloat(num);
-   // int s=Integer.parseInt(num);
-     int summe = 3;
-       /*   while (0 != s) {
-            // addiere die letzte ziffer der uebergebenen zahl zur summe
-            summe = summe + (s % 10);
-            // entferne die letzte ziffer der uebergebenen zahl
-            s = s / 10;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        }*/
+        e = findViewById(R.id.editText);
+        Text = findViewById(R.id.textView);
+        b = findViewById(R.id.button);
+        fabView = findViewById(R.id.fabView);
+fabView.setMovementMethod(new ScrollingMovementMethod());
+
+        calculateDigitSum();
+        calculateFibonacciRow();
+
+
 
 
     }
